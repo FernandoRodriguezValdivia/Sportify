6687d6f3f03ee03e42e28ade97f42a4ccece5ed8
Raul Santa Cruz <raulsantacruz0@gmail.com>
2022-04-22 14:52:43 -0500

    boton busqueda

--- src/Components/NavBar/Search.js
+++ src/Components/NavBar/Search.js
@@ -1,10 +1,19 @@
 import "./Search.css";
+import styled from 'styled-components'
+
+const Button = styled.button`
+background: #aaa;
+border-radius: 15px;
+width: 30px;
+height: 30px;
+`
 
 export default function Search() {
   return (
     <div className="topnav__search-text topnav__center">
-      <i className="fa fa-search"></i>
+      
       <input type="search" className="topnav__buscar" placeholder="Buscar..." />
+      <Button><i className="fa fa-search"></i></Button>
     </div>
   );
 }
